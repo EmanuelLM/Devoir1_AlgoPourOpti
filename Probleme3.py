50dcb6c6d70a636033d9a00341bfa2c1ae0fba29
EmanuelLM <73854070+EmanuelLM@users.noreply.github.com>
2020-11-02 23:38:51 -0500

    Update Probleme3.py

--- Probleme3.py
+++ Probleme3.py
@@ -1,6 +1,6 @@
-L = [3,5,8,9]
-diff = []
-alert = 0 
+L = [3,5,8,9] #Liste de canaux sélectionnés
+diff = [] #Liste contenant les différence entre les paires contenues dans L
+alert = 0 #Variable binaire nous signalant si il y a risque d'interférence 
 for i in range(len(L)):
   for j in range(len(L)): 
     diff.append(abs(L[i]-L[j]))
@@ -9,9 +9,9 @@ for i in range(len(diff)):
   x = 0
   checking = diff[i]
   for j in range(len(diff)) :
-    if checking == diff[j] and checking != 0 :
+    if checking == diff[j] and checking != 0 : #On va calculer la différence entre tous les nombres donc il ne faut pas compter les différence nulles 
       x = x+1
-      if x > 2 :
+      if x > 2 : #Comme la valeur absolue est la même pour une paire, la contrainte sera violée quand on dépassera le doublon dans diff[] (plus de 2 différences identiques)
         alert = 1 
       
 if alert == 1 :
