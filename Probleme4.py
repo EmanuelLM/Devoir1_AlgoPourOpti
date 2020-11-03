a7c3cf64ed88a598323c69e8780bfadac3716eca
EmanuelLM <73854070+EmanuelLM@users.noreply.github.com>
2020-11-02 23:35:07 -0500

    Update Probleme4.py

--- Probleme4.py
+++ Probleme4.py
@@ -4,6 +4,7 @@ Order = []
 maxIndex = []
 maxDiff = -1000
 
+#Trouver la plus grande différence dans les paires contenues dans L 
 for i in range(len(L)):
   for j in range(len(L)):
     a = abs(L[i]-L[j])
@@ -12,7 +13,8 @@ for i in range(len(L)):
       maxDiff = a
       maxIndex.append(i)
       maxIndex.append(j)
-
+      
+#Les rajouter dans notre liste ordonnée et la supprimer de notre liste temporaire 
 Order.append(L[maxIndex[0]])
 Order.append(L[maxIndex[1]])
 LCopy.remove(L[maxIndex[0]])
@@ -20,6 +22,7 @@ LCopy.remove(L[maxIndex[1]])
 maxDiff = -1000
 maxIndex.clear()
 
+#Trouver la plus grande différence entre le dernier nombre dans la liste ordonnée et les nombres restants dans la liste temporaire 
 while len(LCopy) != 0:
   for i in range(len(LCopy)) : 
     a = abs(Order[len(Order)-1] - LCopy[i])
